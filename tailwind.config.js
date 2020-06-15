92fb3cba5df979e218290e81ab19695aa15de93d
Lucas Livchits <livchitsl@gmail.com>
2020-06-15 19:16:21 -0300

    Add fonts to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,32 @@
 module.exports = {
   purge: false,
   theme: {
+    fontFamily: {
+      sans: [
+        'Roboto',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      mono: [
+        'Inconsolata',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
+    },
     extend: {},
   },
   variants: {},
