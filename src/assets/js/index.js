fae9a7699234f9862db1f6355df3c2ff31c63410
Lucas Livchits <livchitsl@gmail.com>
2020-07-05 00:17:49 -0300

    Changes highlightLinks function

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,14 +1,11 @@
 //highlight active menu
-const navbar = document.querySelector('nav');
-const anchors = [...navbar.querySelectorAll('a')].filter(
-  (anchor) => !anchor.href.includes('mailto')
-);
-
 function highlightLinks() {
-  const hash = window.location.hash;
+  const actualSection = window.location.hash;
+
+  const anchors = [...document.querySelectorAll('nav a')];
 
   anchors.forEach((anchor) => {
-    anchor.hash === hash
+    anchor.hash === actualSection
       ? anchor.classList.add('border-current')
       : anchor.classList.remove('border-current');
   });
@@ -37,6 +34,7 @@ navLinks.addEventListener('click', (e) => {
 const projects = document.getElementById('projects');
 const bio = document.getElementById('bio');
 const projectsBackgroundColor = getComputedStyle(projects)['background-color'];
+const navbar = document.querySelector('nav');
 const navbarDefaultBgColor = getComputedStyle(navbar)['background-color'];
 
 document.addEventListener('scroll', () => {
